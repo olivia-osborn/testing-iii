7cf57f616bac2f2e9a6070913dce33da9b5af7cd
Olivia Osborn <olioz95@gmail.com>
2019-03-06 17:47:12 -0500

    tested display to make sure it says open/unlocked accordingly

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -6,13 +6,35 @@ import "jest-dom/extend-expect";
 import Dashboard from "./Dashboard";
 
 describe("toggleLocked()", () => {
-  it("toggles lock status when button is clicked", () => {
+  it("toggles lock status when button is clicked and door is closed", () => {
     const { getByText } = render(<Dashboard />);
     const lockButton = getByText(/lock gate/i);
     const closeButton = getByText(/close gate/i);
     fireEvent.click(closeButton);
+    expect(getByText(/unlocked/i)).toBeInTheDocument();
+    expect(getByText(/closed/i)).toBeInTheDocument();
     fireEvent.click(lockButton);
+    expect(getByText(/unlock gate/i)).toBeInTheDocument();
+    expect(getByText(/open gate/i)).toBeInTheDocument();
+    expect(getByText(/locked/i)).toBeInTheDocument();
+    expect(getByText(/closed/i)).toBeInTheDocument();
+  });
 
-    getByText(/unlock gate/i);
+  it("can't be clicked when gate is open", () => {
+    const { getByText } = render(<Dashboard />);
+    const lockButton = getByText(/lock gate/i);
+    fireEvent.click(lockButton);
+    expect(getByText(/unlocked/i)).toBeInTheDocument();
+  });
+});
+
+describe("toggleClosed()", () => {
+  it("toggles closed status when button is clicked and door is unlocked", () => {
+    const { getByText } = render(<Dashboard />);
+    const closeButton = getByText(/close gate/i);
+    fireEvent.click(closeButton);
+    expect(getByText(/open gate/i)).toBeInTheDocument();
+    fireEvent.click(closeButton);
+    expect(getByText(/close gate/i)).toBeInTheDocument();
   });
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
-// Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+
+import Display from "./Display";
+
+describe("<Display />", () => {
+  it("starts as unlocked and open", () => {
+    const { getByText } = render(<Display />);
+    getByText(/unlocked/i);
+    getByText(/open/i);
+  });
+});
