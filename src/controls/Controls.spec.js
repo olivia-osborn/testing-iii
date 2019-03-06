29746dabd8e4b0c3709e701dbd3f7dbe7a4952cc
Olivia Osborn <olioz95@gmail.com>
2019-03-06 17:21:13 -0500

    tested toggleLocked function

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,13 +1,22 @@
 // Test away!
 import React from "react";
 import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+
 import Controls from "./Controls";
 
 describe("<Controls />", () => {
-  it("should call toggleLock function", () => {
+  it("should call toggleLock function when lock gate is clicked", () => {
     const toggle = jest.fn();
     const { getByText } = render(<Controls toggle={toggle} />);
     fireEvent.click(getByText(/lock gate/i));
+    expect(toggle).toHaveBeenCalled;
+  });
+
+  it("should call toggleClosed function when open gate is clicked", () => {
+    const toggle = jest.fn();
+    const { getByText } = render(<Controls toggle={toggle} />);
+    fireEvent.click(getByText(/close gate/i));
     expect(toggle).toHaveBeenCalled;
   });
 });
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+
+import Dashboard from "./Dashboard";
+
+describe("toggleLocked()", () => {
+  it("toggles lock status when button is clicked", () => {
+    const { getByText } = render(<Dashboard />);
+    const lockButton = getByText(/lock gate/i);
+    const closeButton = getByText(/close gate/i);
+    fireEvent.click(closeButton);
+    fireEvent.click(lockButton);
+
+    getByText(/unlock gate/i);
+  });
+});
