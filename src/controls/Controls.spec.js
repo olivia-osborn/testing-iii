d78c8e8ceea879dd1bc40993590a887d6424f73c
Olivia Osborn <olioz95@gmail.com>
2019-03-06 16:35:39 -0500

    added test for clicking lock gate button

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import Controls from "./Controls";
+
+describe("<Controls />", () => {
+  it("should call toggleLock function", () => {
+    const toggle = jest.fn();
+    const { getByText } = render(<Controls toggle={toggle} />);
+    fireEvent.click(getByText(/lock gate/i));
+    expect(toggle).toHaveBeenCalled;
+  });
+});
